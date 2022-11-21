e50bdf2c7194c4a5295183cf084134b019ba0e60
Andy Ramaroson <andyramaroson@gmail.com>
2022-11-21 16:25:23 +0100

    [documentation] add readMe (Eng)

--- sportsee/Client/src/components/Layout.jsx
+++ sportsee/Client/src/components/Layout.jsx
@@ -1,24 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom'
 
-import Home from "./../pages/Home/Home";
-import PageNotFound from "./../pages/PageNotFound/PageNotFound";
+import Home from './../pages/Home/Home'
+import PageNotFound from './../pages/PageNotFound/PageNotFound'
 
-import Navbar from "./UI/Navbar/Navbar";
-import LeftBar from "./UI/LeftBar/LeftBar";
-import "./UI/Variables.scss"
+import Navbar from './UI/Navbar/Navbar'
+import LeftBar from './UI/LeftBar/LeftBar'
+import './UI/Variables.scss'
 
 function Layout() {
   return (
-    <div className="Layout" >
+    <div className="Layout">
       <Navbar />
       <LeftBar />
       <Routes>
         {/* <Route path="/" element={<Accueil />} /> */}
-        <Route path="/user/:id" element={ <Home /> }></Route>
+        <Route path="/user/:id" element={<Home />}></Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </div> 
+    </div>
   )
 }
 
-export default Layout;
+export default Layout
